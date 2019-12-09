2cc348f40b27fcf0b82c55a5c6b10ae756097654
王炜莉 <wangweili@cmiot.chinamobile.com>
2019-12-09 10:13:03 +0800

    初始化

---
+++ src/main/java/com/example/demo/WwlStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwlStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwlStudyApplication.class, args);
+	}
+
+}
